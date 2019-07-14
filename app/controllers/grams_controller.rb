bd22272a2ef9d481cfaaa526968c2a617fe9c4f7
Michael Lu <michael_108@hotmail.com>
2019-07-14 18:49:38 +0000

    destroy grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,6 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
    def new
     @gram = Gram.new
    end
@@ -26,6 +27,24 @@ class GramsController < ApplicationController
     end
    end
 
+   def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+       redirect_to root_path
+    else
+       return render :edit, status: :unprocessable_entity
+    end
+   end
+
+   def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+   end
+
    private
 
    def gram_params
@@ -35,5 +54,5 @@ class GramsController < ApplicationController
    def render_not_found
     render plain: 'Not Found :(', status: :not_found
   end
-  
+
 end
