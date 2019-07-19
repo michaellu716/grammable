29e68136a25ba0bc1a66aa5da2fc45d465221f59
Michael Lu <michael_108@hotmail.com>
2019-07-19 12:45:56 +0000

    secure,edit,update, destroy, all 21 tests passing

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -26,6 +26,8 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should return a 404 message if we cannot find a gram with the id that is specified" do
+        user = FactoryBot.create(:user)
+        sign_in user
         delete :destroy, params: { id: 'SPACEDUCK' }
         expect(response).to have_http_status(:not_found)
     end
